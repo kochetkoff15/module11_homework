b7418e68e8db085a9c4ef534809109f634348727
Vladimir <mr.kochetkov2018@yandex.ru>
2023-07-03 22:47:31 +0300

    dobavil kod

---
+++ numder1.js
@@ -0,0 +1,10 @@
+const result = +prompt('Введите число', '')
+if (typeof +result !== 'number') {
+  alert('Упс, кажется, вы ошиблись')
+} else if (isNaN(result % 2)) {
+  alert('Ошибка')
+} else if (result % 2 === 0) {
+  alert('Число четное')
+} else {
+  alert('число нечетное')
+}
